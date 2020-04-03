7ff29a2c917b8a447d7043c5b643657fda107a42
Brian Alvarez <soii.dokiee@hotmail.com>
2020-04-03 01:00:38 -0300

    Subida desafio TP2A

---
+++ desafio1/AlvarezBrian.js
@@ -0,0 +1,17 @@
+// Se asume que la duracion de los videos es en minutos y segundos y se pide todo en segundos
+const str = require('./data.js');
+//Se separan por renglones y luego se filtra por aquellos que contengan Flexbox para tener solo esos valores  
+let auxAr = str.split("\n")
+               .filter(p => p.includes('Flexbox'));
+var regexSec = new RegExp('\"(.*)\"'); //Se utiliza para encontrar todo lo que se halle dentro de los caracteres " "
+//Se arma el map (0 : [ '5', '17' ])
+let mapper = auxAr.map(function(i){
+    return i.match(regexSec)[1]
+            .split(":");
+});
+//Obtenemos los minutos por separado y se multiplican por 60 segundos
+var minutos = mapper.map(x => parseInt(x) * 60).reduce((a,b) => a+b);    
+//Obtenemos los segundos
+var segundos = mapper.reduce((acum, vActual) => parseInt(acum) + parseInt(vActual[1]));
+//Salida por pantalla
+console.log(`El total de segundos calculados para Flexbox Video es  ${minutos + segundos}`);  
